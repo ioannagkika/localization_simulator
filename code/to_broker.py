fdabdf127f7ec7b298e2ec8734e01c3a7cfb03e3
Ioanna Gkika <62180847+ioannagkika@users.noreply.github.com>
2023-06-15 17:25:02 +0300

    Update to_broker.py

--- code/to_broker.py
+++ code/to_broker.py
@@ -9,9 +9,9 @@ import numpy as np
 
 class messages():
 
-    def __init__(self, lat = 1, long = 1, heading = 0, dateandtime = datetime(2023, 5, 11, 12, 36, 57, 643401), 
+    def __init__(self, lat = None, long = None, heading = None, dateandtime = datetime(2023, 5, 11, 12, 36, 57, 643401), 
                  PublishingTopic= "hi", json_msg = {}, brokerip = '127.0.0.1' , frid = 'FR003', ToolID = 'LOC-SELF', 
-                 sourceid = "FR001#FR", quality = None, quality_heading = None, outdoor = None, mounting = "helmet") -> None:
+                 sourceid = "FR001#FR", quality = None, quality_heading = None, outdoor = None, mounting = None, altitude = None) -> None:
         #self.tool = tool
         self.brokerip = brokerip
         self.frid = frid
@@ -29,6 +29,7 @@ class messages():
         self.outdoor = outdoor
         self.mounting = mounting
         self.heading = heading
+        self.altitude = altitude
     
 
     def create_json(self):
@@ -36,7 +37,6 @@ class messages():
         if self.ToolID == 'LOC-SELF':
             #ToolName ='Visual based self-Localization'
             category = "VisualSelfLoc#FRLocation"
-
             types = 'SelfLocData'
 
 
@@ -89,14 +89,14 @@ class messages():
         # json_indata['source'] = json_source
 
         json_tooldata={}
-        json_tooldata['latitude'] = self.lat
-        json_tooldata['longitude'] = self.long
-        json_tooldata['heading'] = self.heading
-        json_tooldata['altitude'] = 0
-        json_tooldata['quality'] = self.quality
-        json_tooldata['qualityHeading'] = self.quality_heading
-        json_tooldata['outdoor'] = self.outdoor
-        json_tooldata['mounting'] = self.mounting
+        if self.lat != None: json_tooldata['latitude'] = self.lat
+        if self.long != None: json_tooldata['longitude'] = self.long
+        if self.heading != None: json_tooldata['heading'] = self.heading
+        if self.altitude != None: json_tooldata['altitude'] = self.altitude
+        if self.mounting != None: json_tooldata['mounting'] = self.mounting
+        if self.quality != None: json_tooldata['quality'] = self.quality
+        if self.quality_heading != None: json_tooldata['qualityHeading'] = self.quality_heading
+        if self.outdoor != None: json_tooldata['outdoor'] = self.outdoor
         
         json_infoprioPayload['toolData'] =  [json_tooldata]    
         self.json_msg['infoprioPayload'] = json_infoprioPayload
@@ -109,7 +109,7 @@ class messages():
         date_visual = self.dateandtime
         for i in range(len(visual_latitude)):
             mqtt_visual.append(messages(ToolID = 'LOC-SELF', lat = visual_latitude[i], long = visual_longitude[i], heading = visual_heading[i],
-                                        dateandtime = date_visual, sourceid = sourceid, brokerip=brokerip))
+                                        altitude = 0, mounting = "helmet", dateandtime = date_visual, sourceid = sourceid, brokerip=brokerip))
             mess_visual.append([(mqtt_visual[i].create_json())])
             date_visual = date_visual + timedelta(seconds = visual_timediff)
         mess_visual = (mqtt_visual[0].PublishingTopic, mess_visual, visual_timediff)
@@ -124,7 +124,7 @@ class messages():
         for i in range(len(inertio_latitude)):
             mqtt_inertio.append(messages(ToolID = 'LOC-IBL', lat = inertio_latitude[i], long = inertio_longitude[i],  heading = inertio_heading[i],
                                          dateandtime = date_inertio, sourceid=sourceid, brokerip = brokerip,
-                                         quality=1.23456, mounting=None))
+                                         quality=0, altitude=0))
             mess_inertio.append([(mqtt_inertio[i].create_json())])
             date_inertio = date_inertio + timedelta(seconds = inertio_timediff)
 
@@ -140,7 +140,7 @@ class messages():
         for i in range(len(galileo_latitude)):
             mqtt_galileo.append(messages(ToolID = 'LOC-GLT', lat = galileo_latitude[i], long = galileo_longitude[i], heading = galileo_heading[i],
                                          dateandtime = date_galileo, sourceid=sourceid, brokerip = brokerip,
-                                         quality=1.23456, quality_heading = 0, outdoor=True))
+                                         altitude = 0, quality=0, quality_heading = 0, outdoor=True, mounting = "helmet"))
             mess_galileo.append([(mqtt_galileo[i].create_json())])
             date_galileo = date_galileo + timedelta(seconds = galileo_timediff)
         mess_galileo = (mqtt_galileo[0].PublishingTopic, mess_galileo, galileo_timediff)
@@ -155,7 +155,7 @@ class messages():
         for i in range(len(fusion_latitude)):
             mqtt_fusion.append(messages(ToolID = 'LOC-FUSION', lat = fusion_latitude[i], long = fusion_longitude[i], heading = fusion_heading[i],
                                          dateandtime = date_fusion, sourceid=sourceid, brokerip = brokerip,
-                                         quality=1.23456, quality_heading = 0, outdoor=True))
+                                         quality=0, altitude = 0))
             mess_fusion.append([(mqtt_fusion[i].create_json())])
             date_fusion = date_fusion + timedelta(seconds = fusion_timediff)
         mess_fusion = (mqtt_fusion[0].PublishingTopic, mess_fusion, fusion_timediff)
